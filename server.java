80b40cb7deb341797f3170986391ec62205c6476
Manisha <rlmanus31@gmail.com>
2018-03-20 15:00:52 +0530

    modified server

--- server.java
+++ server.java
@@ -1,8 +1,8 @@
 import java.net.*;  
-import java.io.*;  
-class server{  
-public static void main(String args[])throws Exception{  
-ServerSocket ss=new ServerSocket(3333);  
+    import java.io.*;  
+    class MyServer{  
+    public static void main(String args[])throws Exception{  
+    ServerSocket ss=new ServerSocket(3333);  
     Socket s=ss.accept();  
     DataInputStream din=new DataInputStream(s.getInputStream());  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
@@ -11,11 +11,11 @@ ServerSocket ss=new ServerSocket(3333);
     String str="",str2="";  
     while(!str.equals("stop")){  
     str=din.readUTF();  
-     System.out.println("client says: "+str);  
+    System.out.println("client says: "+str);  
     str2=br.readLine();  
     dout.writeUTF(str2);  
     dout.flush();  
-     }  
+    }  
     din.close();  
     s.close();  
     ss.close();  
