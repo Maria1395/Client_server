39cd78a5788cebeed6e832a0cc39c204f32ebac6
Manisha <rlmanus31@gmail.com>
2018-03-20 13:52:08 +0530

    modified server

--- server.java
+++ server.java
@@ -2,3 +2,7 @@ import java.net.*;
 import java.io.*;  
 class server{  
 public static void main(String args[])throws Exception{  
+ServerSocket ss=new ServerSocket(3333);  
+    Socket s=ss.accept();  
+    DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
